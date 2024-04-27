9968bc7ebe5d9967c6fd803cb77e040286d41ad6
barwait <barwait@naver.com>
2024-04-27 16:27:47 +0900

    Add: Prisma MySQL 연동 완료

---
+++ prisma/migrations/20240427072624_create_categories/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE `categories` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(191) NOT NULL,
+    `role` INTEGER NOT NULL,
+    `token` VARCHAR(191) NOT NULL,
+    `refreshToken` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
