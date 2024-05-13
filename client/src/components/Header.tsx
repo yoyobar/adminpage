3a153241328983c52d87503f0908f92d4cdcb990
barwait <barwait@naver.com>
2024-05-13 18:45:31 +0900

    remove: 내부 README.md 자동생성파일 삭제

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -25,7 +25,6 @@ export default function Header({ logout, ROLE }: HeaderProps) {
   return (
     <div className="z-10 bg-slate-50 transition dark:bg-slate-900 w-full pt-2 select-none border-b-2 dark:border-black">
       <div className="flex h-8 gap-4 justify-end items-center mb-2">
-        {ROLE === "ADMIN" && null}
         {ROLE === "USER" && <Button className="w-18 text-center " onClick={darkHandler} text={dark ? "✷" : "☽"} color={dark ? "gray" : "black"} type="button" />}
         <Button className="w-18 mr-4 w-32 " onClick={logout} text="Logout" color={dark ? "gray" : "black"} type="button" />
       </div>
